8f3d7af737ca1f5eb33c9d5cdecde862b228c48e
ktjlee <ktjlee@umich.edu>
2018-07-25 10:51:03 -0700

    initial commit

---
+++ dogfetchr/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  dogfetchr
+//
+//  Created by Katie  Lee on 7/25/18.
+//  Copyright © 2018 SJJ. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class ViewController: UIViewController {
+    
+    var test = LineChartData()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
